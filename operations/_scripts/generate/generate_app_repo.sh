5f589ad8dfdc43caf0bfeb56ee4a536eb4c05d6d
Mick McGrath <mick@bitovi.com>
2022-11-10 11:29:37 -0500

    fix app copy to ansible

--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -11,8 +11,8 @@ echo "$GITHUB_WORKSPACE"
 echo "ls GITHUB_WORKSPACE"
 ls $GITHUB_WORKSPACE
 
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/{$GITHUB_REPO_NAME}"
-cp -rf "$GITHUB_WORKSPACE"/* >> "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/{$REPO_NAME}"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
+cp -rf "$GITHUB_WORKSPACE"/* >> "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
 
-echo "ls GITHUB_ACTION_PATH/operations/deployment/ansible/app/{$GITHUB_REPO_NAME}"
-ls "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/{$GITHUB_REPO_NAME}"
+echo "ls GITHUB_ACTION_PATH/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
+ls "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
