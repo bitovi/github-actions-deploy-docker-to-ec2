9dd59de0d647e44a01487fece9bd289e76b001fb
Mick McGrath <mick@bitovi.com>
2022-11-10 11:27:42 -0500

    debugging app copy to ansible

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -33,6 +33,11 @@ cat $GITHUB_ACTION_PATH/operations/deployment/terraform/terraform.tfvars
 echo "ls GITHUB_ACTION_PATH/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
 ls "$GITHUB_ACTION_PATH/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
 
+
+echo "DEBUGGING"
+exit 0
+
+
 echo "Running BitOps for env: $BITOPS_ENVIRONMENT"
 docker run --rm --name bitops \
 -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -6,6 +6,11 @@ set -e
 echo "In generate_app_repo.sh"
 GITHUB_REPO_NAME=$(echo $GITHUB_REPOSITORY | sed 's/^.*\///')
 
+echo "GITHUB_WORKSPACE"
+echo "$GITHUB_WORKSPACE"
+echo "ls GITHUB_WORKSPACE"
+ls $GITHUB_WORKSPACE
+
 mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/{$GITHUB_REPO_NAME}"
 cp -rf "$GITHUB_WORKSPACE"/* >> "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/{$REPO_NAME}"
 
