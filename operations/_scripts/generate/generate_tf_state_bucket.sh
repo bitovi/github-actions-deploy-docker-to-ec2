46470ce3b8766c5f277e232301b8cfb0fb3548f8
Mick McGrath <mick@bitovi.com>
2022-11-10 10:20:06 -0500

    clean up tf_state_bucket generation debugging

--- operations/_scripts/generate/generate_tf_state_bucket.sh
+++ operations/_scripts/generate/generate_tf_state_bucket.sh
@@ -7,9 +7,6 @@ if [ -z "${TF_STATE_BUCKET}" ]; then
   ORG_NAME=$(echo $GITHUB_REPOSITORY | sed 's/\/.*//')
   REPO_NAME=$(echo $GITHUB_REPOSITORY | sed 's/^.*\///')
   export TF_STATE_BUCKET="${ORG_NAME}-${REPO_NAME}-tf-state"
-  echo "no bucket"
-else
-  echo "has bucket"
 fi
 
 echo "foo($TF_STATE_BUCKET)"
