9958519f438c2d7ebb332eba603cf8fa3fc6cb14
Mick McGrath <mick@bitovi.com>
2022-11-10 11:31:48 -0500

    debugging app copy to ansible

--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -12,7 +12,7 @@ echo "ls GITHUB_WORKSPACE"
 ls $GITHUB_WORKSPACE
 
 mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
-cp -rf "$GITHUB_WORKSPACE"/* >> "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/"
+cp -rf "$GITHUB_WORKSPACE"/* "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/"
 
 echo "ls GITHUB_ACTION_PATH/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
 ls "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
