bf3c2c3034c7a59de44dc033e767112386b7cdaa
Mick McGrath <mick@bitovi.com>
2022-11-15 16:50:51 -0500

    fix envroot

--- operations/deployment/ansible/bitops.before-deploy.d/merge-tf-env.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/merge-tf-env.sh
@@ -2,17 +2,17 @@
 echo "BitOps Ansible before script: Merge Terraform Enviornment Variables..."
 
 # dotenv file
-DOTENV_FILE="${BITOPS_ENVROOT_DIR}/ansible/app.env"
+DOTENV_FILE="${BITOPS_ENVROOT}/ansible/app.env"
 
 # tf dotenv file
-TF_DOTENV_FILE="${BITOPS_ENVROOT_DIR}/terraform/tf.env"
+TF_DOTENV_FILE="${BITOPS_ENVROOT}/terraform/tf.env"
 
 echo "DEBUGGING"
 echo "cat DOTENV_FILE ($DOTENV_FILE)"
 cat $DOTENV_FILE
 
 
-TEMP_DOTENV_FILE="${BITOPS_ENVROOT_DIR}/ansible/tmp.env"
+TEMP_DOTENV_FILE="${BITOPS_ENVROOT}/ansible/tmp.env"
 
 cat $TF_DOTENV_FILE >> $TEMP_DOTENV_FILE
 cat $DOTENV_FILE >> $TEMP_DOTENV_FILE
