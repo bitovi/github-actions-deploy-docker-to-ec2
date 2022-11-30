1a3a60cee491d53403ca6894284f92ef4139ca3d
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-11-30 09:09:36 -0500

    fix syntax error

--- operations/_scripts/generate/generate_identifier_supershort.sh
+++ operations/_scripts/generate/generate_identifier_supershort.sh
@@ -13,7 +13,7 @@ fi
 
 if [ -z "$AWS_RESOURCE_IDENTIFIER" ]; then
   GITHUB_IDENTIFIER="${GITHUB_ORG_NAME}-${GITHUB_REPO_NAME}-${GITHUB_BRANCH_NAME}"
-  GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/operations/_scripts/generate/shorten_identifier.sh ${GITHUB_IDENTIFIER})" 30
+  GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/operations/_scripts/generate/shorten_identifier.sh ${GITHUB_IDENTIFIER} 30)"
 else
   GITHUB_IDENTIFIER="$AWS_RESOURCE_IDENTIFIER"
 fi
