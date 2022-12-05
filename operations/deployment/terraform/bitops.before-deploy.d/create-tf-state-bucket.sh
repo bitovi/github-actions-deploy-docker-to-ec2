d6f5583d310c81134dc04ec5cc54d0955845fd8a
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-05 13:47:19 -0500

    output for tf state bucket

--- operations/deployment/terraform/bitops.before-deploy.d/create-tf-state-bucket.sh
+++ operations/deployment/terraform/bitops.before-deploy.d/create-tf-state-bucket.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo ""
+echo "Creating TF_STATE_BUCKET: $TF_STATE_BUCKET"
 if [ "$AWS_DEFAULT_REGION" == "us-east-1" ]; then
   aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION || true
 else
