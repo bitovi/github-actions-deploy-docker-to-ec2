7657961bf4ea79c2777d798af77162a84a89d95c
LeoDiazL <leo@bitovi.com>
2022-12-08 16:34:29 -0300

    Security group take in consideration ELB port

--- operations/deployment/terraform/security-group.tf
+++ operations/deployment/terraform/security-group.tf
@@ -32,3 +32,14 @@ resource "aws_security_group_rule" "ingress_ssh" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ec2_security_group.id
 }
+
+resource "aws_security_group_rule" "ingress_lb_port" {
+  count       = var.lb_port != var.app_port ? 1 : 0 
+  type        = "ingress"
+  description = "${var.aws_resource_identifier} - Port"
+  from_port   = var.lb_port != "" ? var.lb_port : ( local.fqdn_provided ? 443 : 80 )
+  to_port     = tonumber(var.app_port)
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_security_group.id
+}
