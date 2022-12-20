9ab8639332a49854a27f1d784b2fb8670a3fc92b
LeoDiazL <leo@bitovi.com>
2022-12-20 11:34:48 -0300

    Fixing output file creation

--- operations/deployment/terraform/env-out.tf
+++ operations/deployment/terraform/env-out.tf
@@ -1,12 +1,5 @@
-resource "null_resource" "export-bitops-variables" {
-  provisioner "local-exec" {
-    command = "echo BO_OUT-vm_url=${local.url} >>"
-  }
-}
-
-
 resource "local_file" "export-bitops-variables" {
-  filename = format("%s/%s", abspath(path.root), "bo-out.env")
+  filename = format("%s/%s", abspath(path.root), "/opt/bitops_deployment/bo-out.env")
   content  = <<-EOT
 vm_url=${local.url}
 EOT
