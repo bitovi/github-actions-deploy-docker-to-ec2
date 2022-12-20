36374ecf8e68018ff2a71d0691972bce5db8386f
LeoDiazL <leo@bitovi.com>
2022-12-20 12:55:34 -0300

    Debugging GHA output

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Export variables to GHA
 
-for variable in $(cat $GITHUB_ACTION_PATH/operations/bo-out.env); do echo $variable >> $GITHUB_ENV && echo $variable; done
+for variable in $(cat $GITHUB_ACTION_PATH/operations/bo-out.env); do echo $variable >> $GITHUB_OUTPUT && echo $variable; done
