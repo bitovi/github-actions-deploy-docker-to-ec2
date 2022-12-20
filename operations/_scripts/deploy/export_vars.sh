507470489ff0cd469201395ba708c7b83779a027
LeoDiazL <leo@bitovi.com>
2022-12-20 12:36:55 -0300

    Debugging file location 4

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # Export variables to GHA
 
-find /home/runner/work -iname bo-out.env
-echo "List dirs"
-ls -lah 
-echo "List GHA PATH"
-ls -lah $GITHUB_ACTION_PATH
-echo "LIST ENV_VARS"
-env
-
-for variable in $(cat $GITHUB_WORKSPACE/bo-out.env); do echo $variable >> $GITHUB_ENV; done
+for variable in $(cat $GITHUB_ACTION_PATH/operations/bo-out.env); do echo $variable >> $GITHUB_ENV; done
