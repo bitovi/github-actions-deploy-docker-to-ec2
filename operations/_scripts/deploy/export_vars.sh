7c3a669e8c3c8abd8e59b952516d19c084f17336
LeoDiazL <leo@bitovi.com>
2022-12-23 11:25:49 -0300

    Add check if file exist for env-out

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Export variables to GHA
 
-cat $GITHUB_ACTION_PATH/operations/bo-out.env >> $GITHUB_OUTPUT
+BO_OUT="$GITHUB_ACTION_PATH/operations/bo-out.env"
+
+if [ -f $BO_OUT ]; then
+  cat $BO_OUT >> $GITHUB_OUTPUT
+fi
