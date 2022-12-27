1df8f3ced750005bd88ebc5204b23994dc76d1c9
LeoDiazL <leo@bitovi.com>
2022-12-27 12:47:06 -0300

    Made tf file not fail if empty value passed

--- operations/deployment/terraform/tf-secretdotenv.tf
+++ operations/deployment/terraform/tf-secretdotenv.tf
@@ -1,13 +1,19 @@
 data "aws_secretsmanager_secret_version" "env_secret" {
-  secret_id = "test1-env"
+  count = local.secret_provided ? 1 : 0
+  secret_id = var.aws_secret_env
 }
 
 locals {
-  s3_secret_raw = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.env_secret.secret_string))
-  s3_secret_string = join("\n", [for k, v in local.s3_secret_raw : "${k}=\"${v}\""])
+  s3_secret_raw    = local.secret_provided ? nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.env_secret[0].secret_string)) : {}
+  s3_secret_string = local.secret_provided ? join("\n", [for k, v in local.s3_secret_raw : "${k}=\"${v}\""]) : ""
 }
 
 resource "local_file" "tf-secretdotenv" {
+  count = local.secret_provided ? 1 : 0
   filename = format("%s/%s", abspath(path.root), "tf-secret.env")
-  content = "${local.s3_secret_string}\n"
+  content  = local.secret_provided ? "${local.s3_secret_string}\n" : ""
+}
+
+locals {
+  secret_provided = ( var.aws_secret_env != "" ? true : false )
 }
