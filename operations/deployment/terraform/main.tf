1310aac1a881eff3c9c2a5d9575eb4fe972ae4f9
PhillypHenning <phil8t8@gmail.com>
2023-01-19 16:31:44 -0500

    Adding secret manager + ec2_key_pair to main.yaml

--- operations/deployment/terraform/main.tf
+++ operations/deployment/terraform/main.tf
@@ -3,9 +3,36 @@ resource "tls_private_key" "key" {
   rsa_bits  = 4096
 }
 
+// Creates an ec2 key pair using the tls_private_key.stackstorm_generated_key public key
 resource "aws_key_pair" "aws_key" {
-  key_name   = "${var.aws_resource_identifier}"
+  key_name   = "${var.aws_resource_identifier_supershort}-ec2kp-${random_string.random.result}"
   public_key = tls_private_key.key.public_key_openssh
+}
+
+// Creates a secret manager secret for the operations_stackstorm public key
+resource "aws_secretsmanager_secret" "stackstorm_keys_sm_secret" {
+   name   = "${var.aws_resource_identifier_supershort}-ec2kp-pub-${random_string.random.result}"
+}
+ 
+resource "aws_secretsmanager_secret_version" "stackstorm_keys_sm_secret_version" {
+  secret_id = aws_secretsmanager_secret.stackstorm_keys_sm_secret.id
+  secret_string = <<EOF
+   {
+    "key": "public_key",
+    "value": "${sensitive(tls_private_key.stackstorm_generated_key.public_key_openssh)}"
+   },
+   {
+    "key": "private_key",
+    "value": "${sensitive(tls_private_key.stackstorm_generated_key.private_key_openssh)}"
+   }
+EOF
+}
+
+resource "random_string" "random" {
+  length    = 5
+  lower     = true
+  special   = false
+  numeric   = false
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
