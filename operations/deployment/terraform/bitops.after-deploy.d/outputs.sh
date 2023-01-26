9a478ff513fa28007b49f0377512b9c1e97f3df4
PhillypHenning <phil8t8@gmail.com>
2023-01-26 10:46:03 -0500

    Update Terraform stack destruction condition
    - Update the stack destruction condition in the output script from `STACK_DESTROY` to `TERRAFORM_DESTROY`
    
    [operations/deployment/terraform/bitops.after-deploy.d/outputs.sh]
    - Update the stack destruction condition in the output script from `STACK_DESTROY` to `TERRAFORM_DESTROY`

--- operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
@@ -5,7 +5,7 @@ set -x
 echo "In afterhook - output.sh"
 
 printenv
-if [ "$STACK_DESTROY" != "true" ]; then
+if [ "$TERRAFORM_DESTROY" != "true" ]; then
     terraform output | sed "s/ = /=/g" | sed "s/\"//g" > /opt/bitops_deployment/bo-out.env
     cat /opt/bitops_deployment/bo-out.env
 fi
