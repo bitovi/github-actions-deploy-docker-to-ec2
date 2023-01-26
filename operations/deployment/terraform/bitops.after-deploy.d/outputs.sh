0efedc4c435ce6125f64505ff7c57b0a48d85304
PhillypHenning <phil8t8@gmail.com>
2023-01-26 10:43:35 -0500

    Improve Terraform deployment output
    - Add debugging, environment variables and output message to `outputs.sh`
    - Store terraform outputs to `/opt/bitops_deployment/bo-out.env` and print it
    
    [operations/deployment/terraform/bitops.after-deploy.d/outputs.sh]
    - Set `-x` flag to enable debugging
    - Add printing of environment variables
    - Change the output message
    - Add terraform output to `/opt/bitops_deployment/bo-out.env` and print it

--- operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "In output.sh"
+set -x
 
+echo "In afterhook - output.sh"
+
+printenv
 if [ "$STACK_DESTROY" != "true" ]; then
     terraform output | sed "s/ = /=/g" | sed "s/\"//g" > /opt/bitops_deployment/bo-out.env
     cat /opt/bitops_deployment/bo-out.env
