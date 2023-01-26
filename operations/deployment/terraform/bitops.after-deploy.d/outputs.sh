1f8a144b34faf6ea797f81d45218bd39008ca029
PhillypHenning <phil8t8@gmail.com>
2023-01-26 10:32:25 -0500

    Update terraform output command in deployment script
    - Update `outputs.sh` to use `terraform output` instead of `tf output`
    
    [operations/deployment/terraform/bitops.after-deploy.d/outputs.sh]
    - Change `tf output` to `terraform output` in `outputs.sh`

--- operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
@@ -2,6 +2,6 @@
 
 echo "In output.sh"
 
-tf output | sed "s/ = /=/g" | sed "s/\"//g" > /opt/bitops_deployment/bo-out.env
+terraform output | sed "s/ = /=/g" | sed "s/\"//g" > /opt/bitops_deployment/bo-out.env
 
 cat /opt/bitops_deployment/bo-out.env
