a357627bae342c23aba0a290245a53b9afe2bf2e
PhillypHenning <phil8t8@gmail.com>
2023-02-03 12:10:23 -0500

    Testing `targets` bitops attribute

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# TODO: use templating
-#    provide '.tf.tmpl' files in the 'operations/deployment' repo
-#    and iterate over all of them to provide context with something like jinja
-#    Example: https://github.com/mattrobenolt/jinja2-cli
-#    jinja2 some_file.tmpl data.json --format=json
-
 echo "In generate_bitops_config.sh"
 
 CONFIG_STACK_ACTION="apply"
@@ -15,9 +9,20 @@ if [ "$STACK_DESTROY" == "true" ]; then
   CONFIG_STACK_ACTION="destroy"
 fi
 
-echo "
+targets=
+if [ -n "$TERRAFORM_TARGETS" ]; then
+  targets="targets:\n"
+  # Iterate over the provided comma-delimited string
+  for item in $(echo $TERRAFORM_TARGETS | sed "s/,/ /g"); do
+    # Add the item to the YAML list
+    targets="$targets\t  - $item\n"
+  done
+fi
+
+echo -e "
 terraform:
     cli:
         stack-action: ${CONFIG_STACK_ACTION}
+        $targets
     options: {}
 " >> "${GITHUB_ACTION_PATH}/operations/deployment/terraform/bitops.config.yaml"
