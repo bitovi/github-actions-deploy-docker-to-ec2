df647b0c6ff87dd6d5f5d80a70594e4445ec6b99
PhillypHenning <phil8t8@gmail.com>
2023-02-03 14:09:48 -0500

    debugging `-en`

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -9,20 +9,23 @@ if [ "$STACK_DESTROY" == "true" ]; then
   CONFIG_STACK_ACTION="destroy"
 fi
 
-targets=
+targets_attribute=
 if [ -n "$TERRAFORM_TARGETS" ]; then
-  targets="targets:\n"
+  targets_attribute="targets:"
   # Iterate over the provided comma-delimited string
   for item in $(echo $TERRAFORM_TARGETS | sed "s/,/ /g"); do
     # Add the item to the YAML list
-    targets="$targets\t  - $item\n"
+  targets="$targets 
+      - $item"
   done
 fi
 
-printf "
+targets_attribute="$targets_attribute $targets"
+
+echo -en "
 terraform:
-    cli:
-      stack-action: $CONFIG_STACK_ACTION
-      $targets
-    options: {}
+  cli:
+    stack-action: $CONFIG_STACK_ACTION
+    $targets_attribute
+  options: {}
 " > $GITHUB_ACTION_PATH/operations/deployment/terraform/bitops.config.yaml
