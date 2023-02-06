946db04b50861352d0d0c0adbf34e3ab96c61bf9
PhillypHenning <phil8t8@gmail.com>
2023-02-06 13:28:03 -0500

    removing IPV6 entries..

--- operations/deployment/terraform/efs.tf
+++ operations/deployment/terraform/efs.tf
@@ -79,7 +79,6 @@ resource "aws_security_group" "efs_security_group" {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -88,7 +87,6 @@ resource "aws_security_group" "efs_security_group" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -96,7 +94,6 @@ resource "aws_security_group" "efs_security_group" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
