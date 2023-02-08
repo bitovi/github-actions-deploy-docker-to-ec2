ce0e40a4929dcb74df7311c16e1bb34e6ea95995
PhillypHenning <phil8t8@gmail.com>
2023-02-08 16:06:14 -0500

    Updating workflow to use `awildphil/bitops:nfs`

--- operations/_scripts/generate/generate_bitops_config.sh
+++ operations/_scripts/generate/generate_bitops_config.sh
@@ -9,21 +9,18 @@ if [ "$STACK_DESTROY" == "true" ]; then
   CONFIG_STACK_ACTION="destroy"
 fi
 
-targets_attribute=
+targets_attribute="targets:"
 if [ -n "$TERRAFORM_TARGETS" ]; then
-  targets_attribute="targets:"
   # Iterate over the provided comma-delimited string
   for item in $(echo $TERRAFORM_TARGETS | sed "s/,/ /g"); do
     # Add the item to the YAML list
   targets="$targets 
       - $item"
   done
-  
-  # random_integer.az_select needs to be created before the "full stack" to avoid a potential state dependency locks
-  targets="$targets
-      - random_integer.az_select"
 fi
-
+# random_integer.az_select needs to be created before the "full stack" to avoid a potential state dependency locks
+targets="$targets
+    - random_integer.az_select"
 targets_attribute="$targets_attribute $targets"
 
 echo -en "
