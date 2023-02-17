39f6306d3dfed89bd571515ea86824e2a6acbe15
LeoDiazL <leo@bitovi.com>
2023-02-17 16:37:29 -0300

    Fixing comment

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -69,9 +69,10 @@ docker run --rm --name bitops \
 -e DEFAULT_FOLDER_NAME="_default" \
 -e BITOPS_FAST_FAIL="${BITOPS_FAST_FAIL}" \
 -v $(echo $GITHUB_ACTION_PATH)/operations:/opt/bitops_deployment \
-#bitovi/bitops:2.3.0
 awildphil/bitops:nfs
 BITOPS_RESULT=$?
 echo "::endgroup::"
+
+#bitovi/bitops:2.3.0
 
 exit $BITOPS_RESULT
