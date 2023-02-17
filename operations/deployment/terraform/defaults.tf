64a90e8680effcd85eb5f9f00060369f9be92b97
LeoDiazL <leo@bitovi.com>
2023-02-17 16:06:02 -0300

    Fixing vpc subnets defaults

--- operations/deployment/terraform/defaults.tf
+++ operations/deployment/terraform/defaults.tf
@@ -1,3 +1,18 @@
+data "aws_subnets" "vpc_subnets" {
+  filter {
+    name   = "vpc-id"
+
+    # todo: support a specified vpc id
+    # values = [var.vpc_id ? var.vpc_id : data.aws_vpc.default.id]
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+output "aws_default_subnet_ids" {
+  description = "The subnet ids from the default vpc"
+  value       = data.aws_subnets.vpc_subnets.ids
+}
+
 data "aws_region" "current" {}
 
 data "aws_vpc" "default" {
