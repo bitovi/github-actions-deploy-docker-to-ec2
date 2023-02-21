13c3a639932fb0f4a7dafa8bd7267a58a11ea801
Phil Henning <phil8t8@gmail.com>
2023-02-21 10:46:33 -0500

    Update deploy.sh (#108)

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -70,5 +70,5 @@ docker run --rm --name bitops \
 -e DEFAULT_FOLDER_NAME="_default" \
 -e BITOPS_FAST_FAIL="${BITOPS_FAST_FAIL}" \
 -v $(echo $GITHUB_ACTION_PATH)/operations:/opt/bitops_deployment \
-bitovi/bitops:2.3.0
+bitovi/bitops:2.4.0
 echo "::endgroup::"
