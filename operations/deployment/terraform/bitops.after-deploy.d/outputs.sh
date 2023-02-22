6c1856f32c4e25c4961d7c73537d9bb9dd688e6f
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-02-22 12:13:33 -0300

    Missing pipe

--- operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/outputs.sh
@@ -17,6 +17,6 @@ echo "In afterhook - output.sh"
 
 if [ "$TERRAFORM_DESTROY" != "true" ]; then
     # The sed command will make each variable be in it's line, and in case a list is present, will transform it into a line
-    terraform output sed -e ':a;/["\)]$/!N;s/\n//;ta' -e 's/ *= */=/g;s/[" ,]//g;s/,\([]]\)/\1/g'  > /opt/bitops_deployment/bo-out.env
+    terraform output | sed -e ':a;/["\)]$/!N;s/\n//;ta' -e 's/ *= */=/g;s/[" ,]//g;s/,\([]]\)/\1/g'  > /opt/bitops_deployment/bo-out.env
     cat /opt/bitops_deployment/bo-out.env
 fi
