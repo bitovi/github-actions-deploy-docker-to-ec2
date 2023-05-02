e1899f0e5daf57dd324d48f1d2fde8c8ef1719af
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-05-02 09:49:17 -0300

    New Ansible connection test task

--- operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "BitOps Ansible before script: Waiting for inventory hosts..."
-python $BITOPS_TEMPDIR/_scripts/ansible/wait-for-inventory-hosts.py
+#echo "BitOps Ansible before script: Waiting for inventory hosts..."
+#python $BITOPS_TEMPDIR/_scripts/ansible/wait-for-inventory-hosts.py
