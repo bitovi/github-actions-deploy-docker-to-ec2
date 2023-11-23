cb11eb6c70662a1e760f6f3c4dd91f8c9629ca5e
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-11-23 15:56:30 -0300

    Update generate_identifier_supershort.sh

--- operations/_scripts/generate/generate_identifier_supershort.sh
+++ operations/_scripts/generate/generate_identifier_supershort.sh
@@ -6,6 +6,6 @@ if [ -z "$AWS_RESOURCE_IDENTIFIER" ]; then
   GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/operations/_scripts/generate/generate_identifier.sh)"
   GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/operations/_scripts/generate/shorten_identifier.sh ${GITHUB_IDENTIFIER} 30)"
 else
-  GITHUB_IDENTIFIER="$AWS_RESOURCE_IDENTIFIER"
+  GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/operations/_scripts/generate/shorten_identifier.sh ${AWS_RESOURCE_IDENTIFIER} 30)"
 fi
 echo "$GITHUB_IDENTIFIER" | xargs
